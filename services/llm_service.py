93a11fef08e90f673f5103c6fe3cb95d1a059538
Sujal <99916468+SuJaL1003@users.noreply.github.com>
2026-05-28 11:24:47 +0530

    Update llm_service.py

--- services/llm_service.py
+++ services/llm_service.py
@@ -1,13 +1,19 @@
-from google import genai
+import google.generativeai as genai
 from dotenv import load_dotenv
 import os
 import time
 
-
+# LOAD ENV VARIABLES
 load_dotenv()
 
-client = genai.Client(
+# CONFIGURE GEMINI API
+genai.configure(
     api_key=os.getenv("GEMINI_API_KEY")
+)
+
+# LOAD MODEL
+model = genai.GenerativeModel(
+    "gemini-1.5-flash"
 )
 
 
@@ -19,23 +25,33 @@ def generate_sql_query(prompt):
     for attempt in range(max_retries):
 
         try:
-            response = client.models.generate_content(
-                model="gemini-1.5-flash",
-                contents=prompt
-            )
+
+            response = model.generate_content(prompt)
+
+            # DEBUG PRINT
+            print("FULL RESPONSE:", response)
 
             generated_sql = response.text.strip()
 
-            generated_sql = generated_sql.replace("```sql", "")
-            generated_sql = generated_sql.replace("```", "")
+            # CLEAN SQL OUTPUT
+            generated_sql = generated_sql.replace(
+                "```sql", ""
+            )
+
+            generated_sql = generated_sql.replace(
+                "```", ""
+            )
+
             generated_sql = generated_sql.strip()
+
+            print("GENERATED SQL:", generated_sql)
 
             return generated_sql
 
         except Exception as e:
 
-            print(f"\nAttempt {attempt + 1} failed:")
-            print(e)
+            print(f"\nAttempt {attempt + 1} failed")
+            print("ERROR:", str(e))
 
             time.sleep(5)
 
