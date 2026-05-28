d9246bfe0117c614084a66f9bfbbf127129e3b7f
sujal <sujal.mishra@nagarro.com>
2026-05-28 09:41:49 +0530

    changes

--- database/setup_database.py
+++ database/setup_database.py
@@ -1,11 +1,8 @@
 import sqlite3
-import os
 
-BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 
-db_path = os.path.join(BASE_DIR, "database", "sales_database.db")
+connection = sqlite3.connect("database/sales_database.db")
 
-connection = sqlite3.connect(db_path)
 
 cursor = connection.cursor()
 
